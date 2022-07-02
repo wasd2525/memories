9b15a1a583cd9c919a026f85270171d85f295a91
wasd2525 <hben123@gmail.com>
2022-07-02 20:10:09 +0300

    Add server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
